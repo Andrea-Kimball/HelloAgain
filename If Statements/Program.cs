c7d2b51ac2febb25fc0a39bb84bce218877c67b7
Andrea Kimball <aundreya.7@gmail.com>
2022-02-23 20:55:09 -0500

    If Statement based on the age a user inputs

---
+++ If Statements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int userAge;
+            Console.WriteLine("Please enter your age");
+                userAge = Convert.ToInt32(Console.ReadLine());
+            if (userAge <=0 || userAge >=100)
+            {
+                Console.WriteLine("Please enter a valid age");
+            }
+            else if (userAge >0 || userAge <18)
+            {
+                Console.WriteLine("You must be 18 or older");
+            }
+            else
+            {
+                Console.WriteLine("Your entry could not be recongnized. Please try again");
+            }
+            Console.ReadKey();
+        }
+    }
+}
