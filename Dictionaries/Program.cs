3c0b22efe5fa3db74518d22cf58767115792860c
Andrea Kimball <aundreya.7@gmail.com>
2022-03-01 20:47:34 -0500

    creating a dictionary, calling value from the dictionary with user input, and implementing trygetkey function in the event that the user enters a number that isnt present in the dictionary

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //creating a dictionary
+            Dictionary<int, string> classroom = new Dictionary<int, string>();
+            {
+                classroom.Add(1, "Anne");
+                classroom.Add(2, "Chuck");
+                classroom.Add(3, "Billy");
+                classroom.Add(4, "Mandy");
+                classroom.Add(5, "Claire");
+
+            };
+
+            //calling a value from the dictionary using the key
+            int userInput;
+            Console.WriteLine("Please enter a student ID");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            string value;
+            bool hasValue = classroom.TryGetValue(userInput, out value);
+            if (!hasValue)
+            {
+                Console.WriteLine("That student ID could not be found");
+
+            }
+            else 
+            {
+                Console.WriteLine("student ID {0} belongs to: {1}", userInput, classroom[userInput]);
+            }
+
+            
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
